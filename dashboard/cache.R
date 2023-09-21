4f4c4d92758e2f7e1a007da16f9f0cf051b8e87c
Quinn Thomas <rqthomas@vt.edu>
2023-09-20 21:04:36 -0400

    Update cache.R

--- dashboard/cache.R
+++ dashboard/cache.R
@@ -1,7 +1,7 @@
 #install.packages("remotes")
 #remotes::install_github("cboettig/minio")
 library(minioclient)
-config <- yaml::read_yaml("../challenge_configuration.yaml")
+config <- yaml::read_yaml("challenge_configuration.yaml")
 install_mc()
 mc_alias_set("mc_bucket",  endpoint = config$endpoint,
              access_key = "", secret_key = "")

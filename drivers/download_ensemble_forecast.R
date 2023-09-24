52d3ce155ac4ee4192db24c639a4cb773c6af517
Quinn Thomas <rqthomas@vt.edu>
2023-09-24 15:00:20 -0400

    Update download_ensemble_forecast.R

--- drivers/download_ensemble_forecast.R
+++ drivers/download_ensemble_forecast.R
@@ -1,22 +1,24 @@
 
 download_ensemble_forecast <- function(model){
 
-  s3 <- arrow::s3_bucket("bio230121-bucket01/flare",
+  s3 <- arrow::s3_bucket(bucket = "bio230121-bucket01/flare",
                          endpoint_override = "renc.osn.xsede.org",
                          access_key = Sys.getenv("OSN_KEY"),
                          secret_key = Sys.getenv("OSN_SECRET"))
-
+  
   s3$CreateDir("drivers/met/ensemble_forecast")
-
+  
   s3 <- arrow::s3_bucket("bio230121-bucket01/flare/drivers/met/ensemble_forecast",
                          endpoint_override = "renc.osn.xsede.org",
                          access_key = Sys.getenv("OSN_KEY"),
                          secret_key = Sys.getenv("OSN_SECRET"))
-
+  
   site_list <- readr::read_csv("https://raw.githubusercontent.com/FLARE-forecast/aws_noaa/master/site_list_v2.csv", show_col_types = FALSE)
-
-  for(i in 1:nrow(site_list)){
-
+  
+  for(i in 1:4){
+    
+    print(i)
+    
     RopenMeteo::get_ensemble_forecast(
       latitude = site_list$latitude[i],
       longitude = site_list$longitude[i],
@@ -31,5 +33,7 @@ download_ensemble_forecast <- function(model){
       dplyr::mutate(reference_date = lubridate::as_date(reference_datetime)) |>
       arrow::write_dataset(s3, format = 'parquet',
                            partitioning = c("model_id", "reference_date", "site_id"))
+    Sys.sleep(5)
+    
   }
 }

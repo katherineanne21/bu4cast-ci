a5a45c22d1a315d9577f00a4aac0c0fc72ecedd3
addelany <addelany@gmail.com>
2023-09-28 14:39:18 -0500

    fix date ranges

--- var_stac/forecasts/forecast_models.R
+++ var_stac/forecasts/forecast_models.R
@@ -39,8 +39,8 @@ theme_models <- forecast_data_df |>
   distinct(model_id)
 
 forecast_date_range <- forecast_data_df |> dplyr::summarise(min(date),max(date))
-forecast_max_date <- forecast_date_range$`min(date)`
-forecast_min_date <- forecast_date_range$`max(date)`
+forecast_min_date <- forecast_date_range$`min(date)`
+forecast_max_date <- forecast_date_range$`max(date)`
 
 build_description <- "The catalog contains forecasts for the VERA Forecasting Challenge Daily theme. The forecasts are the raw forecasts that include all ensemble members (if a forecast represents uncertainty using an ensemble).  Due to the size of the raw forecasts, we recommend accessing the scores (summaries of the forecasts) to analyze forecasts (unless you need the individual ensemble members). You can access the forecasts at the top level of the dataset where all models, variables, and dates that forecasts were produced (reference_datetime) are available. The code to access the entire dataset is provided as an asset. Given the size of the forecast catalog, it can be time-consuming to access the data at the full dataset level. For quicker access to the forecasts for a particular model (model_id), we also provide the code to access the data at the model_id level as an asset for each model."
 
@@ -167,8 +167,8 @@ for (i in 1:length(variable_groups)){
       filter(variable == v)
 
     var_date_range <- var_data |> dplyr::summarise(min(date),max(date))
-    var_max_date <- var_date_range$`min(date)`
-    var_min_date <- var_date_range$`max(date)`
+    var_min_date <- var_date_range$`min(date)`
+    var_max_date <- var_date_range$`max(date)`
 
     var_models <- var_data |> distinct(model_id)
 

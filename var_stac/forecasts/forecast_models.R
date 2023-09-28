eca55b909be3fdb673ecc0e8a544db1d9129f387
addelany <addelany@gmail.com>
2023-09-28 10:05:44 -0500

    add new var groups and automate

--- var_stac/forecasts/forecast_models.R
+++ var_stac/forecasts/forecast_models.R
@@ -4,6 +4,10 @@ library(dplyr)
 source('var_stac/R/stac_functions.R')
 config <- yaml::read_yaml('challenge_configuration.yaml')
 
+variable_groups <- c('Biological', 'Physical')
+variable_list <- list(c('Chla_ugL'),
+                      c('Temp_C'))
+
 ## CREATE table for column descriptions
 forecast_description_create <- data.frame(datetime = 'ISO 8601(ISO 2019)datetime the forecast starts from (a.k.a. issue time); Only needed if more than one reference_datetime is stored in a single file. Forecast lead time is thus datetime-reference_datetime. In a hindcast the reference_datetime will be earlier than the time the hindcast was actually produced (see pubDate in Section3). Date times are allowed to be earlier than the reference_datetime if a reanalysis/reforecast is run before the start of the forecast period. This variable was called start_time before v0.5 of the EFI standard.',
                                  site_id = 'For forecasts that are not on a spatial grid, use of a site dimension that maps to a more detailed geometry (points, polygons, etc.) is allowable. In general this would be documented in the external metadata (e.g., alook-up table that provides lon and lat); however in netCDF this could be handled by the CF Discrete Sampling Geometry data model.',
@@ -40,7 +44,7 @@ forecast_min_date <- forecast_date_range$`max(date)`
 
 build_description <- "The catalog contains forecasts for the VERA Forecasting Challenge Daily theme. The forecasts are the raw forecasts that include all ensemble members (if a forecast represents uncertainty using an ensemble).  Due to the size of the raw forecasts, we recommend accessing the scores (summaries of the forecasts) to analyze forecasts (unless you need the individual ensemble members). You can access the forecasts at the top level of the dataset where all models, variables, and dates that forecasts were produced (reference_datetime) are available. The code to access the entire dataset is provided as an asset. Given the size of the forecast catalog, it can be time-consuming to access the data at the full dataset level. For quicker access to the forecasts for a particular model (model_id), we also provide the code to access the data at the model_id level as an asset for each model."
 
-variable_group <- c('test_daily')
+#variable_group <- c('test_daily')
 
 
 build_forecast_scores(table_schema = forecast_theme_df,
@@ -56,7 +60,7 @@ build_forecast_scores(table_schema = forecast_theme_df,
                       model_documentation ="https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv",
                       destination_path = "var_stac/forecasts/",
                       aws_download_path = 'bio230121-bucket01/vera4cast/forecasts/parquet/daily',
-                      link_items = generate_group_values(group_values = variable_group),
+                      link_items = generate_group_values(group_values = variable_groups),
                       thumbnail_link = "https://raw.githubusercontent.com/addelany/vera4cast/main/thumbnails/banner-2.jpg",
                       thumbnail_title = 'Falling Creek Reservoir')
 
@@ -125,3 +129,63 @@ for (m in theme_models$model_id){
               table_description = forecast_description_create)
 }
 
+
+## BUILD VARIABLE GROUPS
+
+for (i in 1:length(variable_groups)){
+  print(variable_groups[i])
+
+  if (!dir.exists(paste0("var_stac/forecasts/",variable_groups[i]))){
+    dir.create(paste0("var_stac/forecasts/",variable_groups[i]))
+  }
+
+  build_group_variables(table_schema = forecast_theme_df,
+                        theme_id = variable_groups[i],
+                        table_description = forecast_description_create,
+                        start_date = forecast_min_date,
+                        end_date = forecast_max_date,
+                        id_value = variable_groups[i],
+                        description_string = build_description,
+                        about_string = 'https://projects.ecoforecast.org/neon4cast-docs/',
+                        about_title = "VERA Forecasting Challenge Documentation",
+                        theme_title = variable_groups[i],
+                        model_documentation ="https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv",
+                        destination_path = paste0("var_stac/forecasts/",variable_groups[i]),
+                        aws_download_path = 'bio230121-bucket01/vera4cast/forecasts/parquet/daily',
+                        group_var_items = generate_group_variable_items(variables = variable_list[[i]]))
+
+  for (v in variable_list[[i]]){ # Make variable JSONS within each group
+    print(v)
+
+    if (!dir.exists(paste0("var_stac/forecasts/",variable_groups[i],'/',v))){
+      dir.create(paste0("var_stac/forecasts/",variable_groups[i],'/',v))
+    }
+
+    var_data <- forecast_data_df |>
+      filter(variable == v)
+
+    var_date_range <- var_data |> dplyr::summarise(min(date),max(date))
+    var_max_date <- var_date_range$`min(date)`
+    var_min_date <- var_date_range$`max(date)`
+
+    var_models <- var_data |> distinct(model_id)
+
+    build_group_variables(table_schema = forecast_theme_df,
+                          theme_id = v,
+                          table_description = forecast_description_create,
+                          start_date = var_min_date,
+                          end_date = var_max_date,
+                          id_value = v,
+                          description_string = build_description,
+                          about_string = 'https://projects.ecoforecast.org/neon4cast-docs/',
+                          about_title = "VERA Forecasting Challenge Documentation",
+                          theme_title = v,
+                          model_documentation ="https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv",
+                          destination_path = file.path("var_stac/forecasts/",i,v),
+                          aws_download_path = var_data$path[1],
+                          group_var_items = generate_variable_model_items(model_list = var_models$model_id))
+
+  }
+
+
+}

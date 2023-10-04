60abe1f9a79a29ce274a8bc269baf79d4ba147cb
rqthomas <rqthomas@vt.edu>
2023-10-04 13:24:59 -0400

    updating targets directory

--- targets/target_generation/daily_targets.R
+++ targets/target_generation/daily_targets.R
@@ -24,7 +24,7 @@ combined_targets <- bind_rows(exo_daily, fluoro_daily)
 
 
 s3 <- arrow::s3_bucket("bio230121-bucket01", endpoint_override = "renc.osn.xsede.org")
-s3$CreateDir("vera4cast/targets/daily")
+s3$CreateDir("vera4cast/targets/duration=P1D")
 
-s3 <- arrow::s3_bucket("bio230121-bucket01/vera4cast/targets/daily", endpoint_override = "renc.osn.xsede.org")
-arrow::write_csv_arrow(exo_daily, sink = s3$path("daily-targets.csv.gz"))
+s3 <- arrow::s3_bucket("bio230121-bucket01/vera4cast/targets/duration=P1D", endpoint_override = "renc.osn.xsede.org")
+arrow::write_csv_arrow(exo_daily, sink = s3$path("P1D-targets.csv.gz"))

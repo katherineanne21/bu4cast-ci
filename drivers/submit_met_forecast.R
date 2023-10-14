584e666f48ce03cfc67d648c8e44236f460263b8
rqthomas <rqthomas@vt.edu>
2023-10-14 14:39:04 -0400

    updating

--- drivers/submit_met_forecast.R
+++ drivers/submit_met_forecast.R
@@ -5,25 +5,24 @@ submit_met_forecast <- function(model_id){
                          access_key = Sys.getenv("OSN_KEY"),
                          secret_key = Sys.getenv("OSN_SECRET"))
 
-  df <- arrow::open_dataset(s3) |> filter(site_id == "fcre") |> collect()
+  df <- arrow::open_dataset(s3) |> dplyr::filter(site_id == "fcre") |> dplyr::collect()
 
   max_reference_date <- max(df$reference_date)
 
-  library(tidyverse)
   filename <- paste0("drivers/", model_id, "-",max_reference_date,".csv.gz")
-  df |> filter(reference_date == max_reference_date) |>
-    mutate(date = lubridate::as_date(datetime)) |>
-    select(-unit) |>
-    pivot_wider(names_from = variable, values_from = prediction) |>
-    summarize(RH_percent_mean = mean(relativehumidity_2m, na.rm = TRUE),
+  df |> dplyr::filter(reference_date == max_reference_date) |>
+    dplyr::mutate(date = lubridate::as_date(datetime)) |>
+    dplyr::select(-unit) |>
+    tidyr::pivot_wider(names_from = variable, values_from = prediction) |>
+    dplyr::summarize(RH_percent_mean = mean(relativehumidity_2m, na.rm = TRUE),
               Rain_mm_sum = sum(precipitation, na.rm = TRUE),
               WindSpeed_ms_mean = mean(windspeed_10m, na.rm = TRUE),
               AirTemp_C_mean = mean(temperature_2m, na.rm = TRUE),
               ShortwaveRadiationUp_Wm2_mean = mean(shortwave_radiation, na.rm = TRUE),
               BP_kPa_mean = mean(surface_pressure * 0.1, na.rm = TRUE),
               .by = c("date","ensemble")) |>
-    pivot_longer(-c(date, ensemble), names_to = "variable", values_to = "prediction") |>
-    mutate(datetime = lubridate::as_datetime(date),
+    tidyr::pivot_longer(-c(date, ensemble), names_to = "variable", values_to = "prediction") |>
+    dplyr::mutate(datetime = lubridate::as_datetime(date),
            reference_datetime = lubridate::as_datetime(max_reference_date),
            site_id = "fcre",
            model_id = model_id,
@@ -32,8 +31,8 @@ submit_met_forecast <- function(model_id){
            depth_m = NA,
            family = "ensemble",
            ensemble = as.numeric(ensemble)) |>
-    rename(parameter = ensemble) |>
-    select(c("project_id", "site_id","model_id", "reference_datetime", "datetime","duration", "depth_m","variable", "family", "parameter", "prediction")) |>
+    dplyr::rename(parameter = ensemble) |>
+    dplyr::select(c("project_id", "site_id","model_id", "reference_datetime", "datetime","duration", "depth_m","variable", "family", "parameter", "prediction")) |>
     readr::write_csv(filename)
 
   vera4castHelpers::submit(filename, first_submission = FALSE)

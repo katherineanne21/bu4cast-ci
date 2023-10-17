fd672af4d603c6c5e55091261aaed10fa681f212
rqthomas <rqthomas@vt.edu>
2023-10-17 11:34:51 -0400

    adding graph of model types to demo use of model metadata

--- R/access_model_metadata.R
+++ R/access_model_metadata.R
@@ -3,4 +3,12 @@ s3 <- arrow::s3_bucket(bucket = "bio230121-bucket01/vera4cast/metadata/model_id/
 
 d1 <- arrow::open_dataset(s3, format = "json") |> dplyr::collect()
 
-unnest(d1[[3]], cols = names(d1[[3]]))
+model_type <- unnest(d1[[3]], cols = names(d1[[3]]))$type
+
+model_type[which(stringr::str_detect(model_type, "mpirical"))] <- "Empirical"
+
+tibble::tibble(model_type = model_type) |>
+ggplot(aes(x = model_type)) +
+  geom_bar() +
+  labs(x = "Model Type", y = "Number submitting forecasts") +
+  theme_bw()

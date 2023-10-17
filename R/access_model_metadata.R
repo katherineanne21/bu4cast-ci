45fee4854913cdf6bde4607cb80d5039bf9bdb13
rqthomas <rqthomas@vt.edu>
2023-10-17 11:14:52 -0400

    adding code to access model metadata

---
+++ R/access_model_metadata.R
@@ -0,0 +1,7 @@
+s3 <- arrow::s3_bucket(bucket = "bio230121-bucket01/vera4cast/metadata/model_id/",
+                       endpoint_override = "renc.osn.xsede.org", anonymous = TRUE)
+
+
+d1 <- df |> dplyr::collect()
+
+unnest(d1[[3]], cols = names(d1[[3]]))

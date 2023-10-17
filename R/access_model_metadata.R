70dadee66084860f88a06ee0edaa532b28bb0aac
rqthomas <rqthomas@vt.edu>
2023-10-17 11:17:53 -0400

    fixing json read

--- R/access_model_metadata.R
+++ R/access_model_metadata.R
@@ -1,7 +1,6 @@
 s3 <- arrow::s3_bucket(bucket = "bio230121-bucket01/vera4cast/metadata/model_id/",
                        endpoint_override = "renc.osn.xsede.org", anonymous = TRUE)
 
-
-d1 <- df |> dplyr::collect()
+d1 <- arrow::open_dataset(s3, format = "json") |> dplyr::collect()
 
 unnest(d1[[3]], cols = names(d1[[3]]))

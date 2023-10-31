d34f63413b0581e18ab42655da6de6bcdfee91de
rqthomas <rqthomas@vt.edu>
2023-10-31 16:08:18 -0400

    initial upload of neon4cast stuff

--- submission_processing/process_submissions.R
+++ submission_processing/process_submissions.R
@@ -1,4 +1,4 @@
-library(vera4castHelpers) #project_specific
+library(neon4cast) #project_specific
 
 
 library(readr)
@@ -17,14 +17,14 @@ install_mc()
 config <- yaml::read_yaml("challenge_configuration.yaml")
 
 minioclient::mc_alias_set("s3_store",
-             config$endpoint,
-             Sys.getenv("OSN_KEY"),
-             Sys.getenv("OSN_SECRET"))
+                          config$endpoint,
+                          Sys.getenv("OSN_KEY"),
+                          Sys.getenv("OSN_SECRET"))
 
 minioclient::mc_alias_set("submit",
-             config$submissions_endpoint,
-             Sys.getenv("AWS_ACCESS_KEY_SUBMISSIONS"),
-             Sys.getenv("AWS_SECRET_ACCESS_KEY_SUBMISSIONS"))
+                          config$submissions_endpoint,
+                          Sys.getenv("AWS_ACCESS_KEY_SUBMISSIONS"),
+                          Sys.getenv("AWS_SECRET_ACCESS_KEY_SUBMISSIONS"))
 
 message(paste0("Starting Processing Submissions ", Sys.time()))
 
@@ -69,28 +69,33 @@ if(length(submissions) > 0){
   for(i in 1:length(submissions)){
 
     curr_submission <- basename(submissions[i])
+    theme <-  stringr::str_split(curr_submission, "-")[[1]][1]
     submission_dir <- dirname(submissions[i])
     print(curr_submission)
 
-    if((tools::file_ext(curr_submission) %in% c("gz", "csv"))){
+    if((tools::file_ext(curr_submission) %in% c("gz", "csv", "nc"))){
 
       valid <- forecast_output_validator(file.path(local_dir, curr_submission))
 
       if(valid){
 
-        fc <- readr::read_csv(submissions[i], show_col_types = FALSE)
+        fc <- read4cast::read_forecast(submissions[i])
 
         pub_datetime <- strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
-        if("depth_m" %in% names(fc)){
-          fc <- fc |> dplyr::mutate(depth_m = as.numeric(depth_m))
-        }
-
         if(!"duration" %in% names(fc)){
-          if(stringr::str_detect(fc$datetime[1], ":")){
+          if(theme == "terrestrial_30min"){
             fc <- fc |> dplyr::mutate(duration = "P1H")
-          }else{
+          }else if(theme %in% c("ticks","beetles")){
+            fc <- fc |> dplyr::mutate(duration = "P1W")
+          }else if(theme %in% c("aquatics","phenology","terrestrial_daily")){
             fc <- fc |> dplyr::mutate(duration = "P1D")
+          }else{
+            if(stringr::str_detect(fc$datetime[1], ":")){
+              fc <- fc |> dplyr::mutate(duration = "P1H")
+            }else{
+              fc <- fc |> dplyr::mutate(duration = "P1D")
+            }
           }
         }
 
@@ -98,23 +103,24 @@ if(length(submissions) > 0){
           dplyr::mutate(pub_datetime = lubridate::as_datetime(pub_datetime),
                         reference_datetime = lubridate::as_datetime(reference_datetime),
                         reference_date = lubridate::as_date(reference_datetime),
-                        parameter = as.character(parameter))
+                        parameter = as.character(parameter),
+                        project_id = "neon4cast")
 
         print(head(fc))
         s3$CreateDir(paste0("parquet/"))
         path <- s3$path(paste0("parquet/"))
         fc |> arrow::write_dataset(path, format = 'parquet',
-                            partitioning = c("project_id",
-                                             "duration",
-                                             "variable",
-                                             "model_id",
-                                             "reference_date"))
+                                   partitioning = c("project_id",
+                                                    "duration",
+                                                    "variable",
+                                                    "model_id",
+                                                    "reference_date"))
 
         bucket <- config$forecasts_bucket
         curr_inventory <- fc |>
           dplyr::mutate(date = lubridate::as_date(datetime),
-                 path = glue::glue("{bucket}/parquet/project_id={project_id}/duration={duration}/variable={variable}"),
-                 endpoint = config$endpoint) |>
+                        path = glue::glue("{bucket}/parquet/project_id={project_id}/duration={duration}/variable={variable}"),
+                        endpoint = config$endpoint) |>
           dplyr::distinct(project_id, duration, model_id, site_id, reference_date, variable, date, path, endpoint)
 
         inventory_df <- dplyr::bind_rows(inventory_df, curr_inventory)

1c762085deba97965bc28dec185f6d329b9b867f
rqthomas <rqthomas@vt.edu>
2023-11-10 15:17:46 -0500

    adding scores inventory

--- R/rebuild_inventory.R
+++ R/rebuild_inventory.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 config <- yaml::read_yaml("challenge_configuration.yaml")
 
+rebuild_forecasts <- FALSE
+rebuild_scores <- TRUE
+
+if(rebuild_forecasts){
 s3 <- arrow::s3_bucket(paste0(config$forecasts_bucket, "/parquet"), endpoint_override = config$endpoint, anonymous = TRUE)
 
 bucket <- config$forecasts_bucket
@@ -34,8 +38,11 @@ s3_inventory <- arrow::s3_bucket(config$inventory_bucket,
 
 inventory_df |> distinct(model_id, project_id) |>
   arrow::write_csv_arrow(s3_inventory$path("model_id/model_id-project_id-inventory.csv"))
+}
 
 ###
+
+if(rebuild_scores){
 
 library(tidyverse)
 config <- yaml::read_yaml("challenge_configuration.yaml")
@@ -52,6 +59,12 @@ inventory_df <- arrow::open_dataset(s3) |>
          path_full = glue::glue("{bucket}/parquet/project_id={project_id}/duration={duration}/variable={variable}/model_id={model_id}/date={date}/part-0.parquet"),
          endpoint =config$endpoint)
 
+sites <- readr::read_csv(config$site_table,show_col_types = FALSE) |>
+  select(field_site_id, latitude, longitude) |>
+  rename(site_id = field_site_id)
+
+inventory_df <- dplyr::left_join(inventory_df, sites, by = "site_id")
+
 s3_inventory <- arrow::s3_bucket(config$inventory_bucket,
                                  endpoint_override = config$endpoint,
                                  access_key = Sys.getenv("OSN_KEY"),
@@ -59,8 +72,7 @@ s3_inventory <- arrow::s3_bucket(config$inventory_bucket,
 
 arrow::write_dataset(inventory_df, path = s3_inventory$path(glue::glue("catalog/scores/project_id={config$project_id}")))
 
-
-
+}
 
 
 

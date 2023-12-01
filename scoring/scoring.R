8f734b6e2b330286361a7c507707cbf7d7cdcdbb
rqthomas <rqthomas@vt.edu>
2023-12-01 16:00:08 -0500

    cleaning up

--- scoring/scoring.R
+++ scoring/scoring.R
@@ -108,7 +108,7 @@ furrr::future_walk(1:nrow(variable_duration), function(k, variable_duration, con
 
       group <- groupings[j,]
       ref <- group$date
-               
+
       tg <- target |>
         #dplyr::mutate(depth_m = ifelse(!is.na(depth_m), round(depth_m, 2), depth_m)) |>  #project_specific
         dplyr::filter(lubridate::as_date(datetime) >= ref,

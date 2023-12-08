9ae73607318c1af918f0c63ff0c400163ca2d6b7
addelany <addelany@gmail.com>
2023-12-08 09:08:07 -0600

    add garbage collection

--- catalog/update_stac.R
+++ catalog/update_stac.R
@@ -18,36 +18,42 @@ print('BUILDING FORECASTS')
 source('catalog/forecasts/forecast_models.R')
 
 rm(list = ls()) # remove all environmental vars between forecast and scores
+gc()
 
 # scores
 print('BUILDING SCORES')
 source('catalog/scores/scores_models.R')
 
 rm(list = ls())
+gc()
 
 # inventory
 print('BUILDING INVENTORY')
 source('catalog/inventory/create_inventory_page.R')
 
 rm(list = ls())
+gc()
 
 # summaries
 print('BUILDING FORECAST SUMMARIES')
 source('catalog/summaries/summaries_models.R')
 
 rm(list = ls())
+gc()
 
 # targets
 print('BUILDING TARGETS')
 source('catalog/targets/create_targets_page.R')
 
 rm(list = ls())
+gc()
 
 # noaa
 print('BUILDING NOAA')
 source('catalog/noaa_forecasts/noaa_forecasts.R')
 
 rm(list = ls())
+gc()
 
 # sites
 print('BUILDING SITES')

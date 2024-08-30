0c941f5dd01e0c67f5292b0ea39dbcb97a7a0beb
rqthomas <rqthomas@vt.edu>
2024-08-30 14:03:47 -0400

    moving dashboard to the bundles

--- dashboard/cache.R
+++ dashboard/cache.R
@@ -4,6 +4,6 @@ install_mc()
 mc_alias_set("mc_bucket",  endpoint = config$endpoint,
              access_key = "", secret_key = "")
 
-mc(paste0("mirror --overwrite mc_bucket/",config$scores_bucket,"/parquet/project_id=", config$project_id," cache/scores"))
+mc(paste0("mirror --overwrite mc_bucket/",config$scores_bucket,"/bundled-parquet/project_id=", config$project_id," cache/scores"))
 
-mc(paste0("mirror --overwrite mc_bucket/",config$forecasts_bucket,"/summaries/project_id=", config$project_id," cache/summaries"))
+mc(paste0("mirror --overwrite mc_bucket/",config$forecasts_bucket,"/bundled-summaries/project_id=", config$project_id," cache/summaries"))

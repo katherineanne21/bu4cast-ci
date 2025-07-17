6d73cc8c102c6549e6f7b6c4c0e85eb5a7cf05dc
Carl Boettiger <cboettig@gmail.com>
2025-07-17 22:24:39 +0000

    :see_no_evil: working bundling?

--- scoring/bundled-forecasts.R
+++ scoring/bundled-forecasts.R
@@ -11,73 +11,86 @@ library(bench)
 library(glue)
 library(fs)
 
+install_mc()
+mc_alias_set("osn", "sdsc.osn.xsede.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
+# mc_alias_set("nrp", "s3-west.nrp-nautilus.io", Sys.getenv("EFI_NRP_KEY"), Sys.getenv("EFI_NRP_SECRET"))
+
+duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
+
+
 open_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-parquet",
              s3_endpoint = "sdsc.osn.xsede.org",
              anonymous = TRUE) |>
   count()
-
-install_mc()
-mc_alias_set("osn", "sdsc.osn.xsede.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
-#mc_alias_set("nrp", "s3-west.nrp-nautilus.io", Sys.getenv("EFI_NRP_KEY"), Sys.getenv("EFI_NRP_SECRET"))
 
 # make sure new-forecasts location exists and is empty.
 fs::dir_create("new-forecasts"); fs::dir_delete("new-forecasts")
 fs::dir_create("forecasts/parquet")
 fs::dir_create("new-forecasts/bundled-parquet")
 
-# Sync local scores, fastest way(?)
-# We could avoid this.  We could stream from both of these.
-# Would still need to create new-forecasts locally and mc_mirror them back with overwrite but not remove.
 
-bench::bench_time({
-  # mirror everything(!) crazy
-  mc_mirror("osn/bio230014-bucket01/challenges/forecasts/parquet/", "forecasts/parquet/", overwrite = TRUE, remove=TRUE)
-  mc_mirror("osn/bio230014-bucket01/challenges/forecasts/bundled-parquet/", "forecasts/bundled-parquet/", overwrite = TRUE, remove=TRUE)
+## Stream data, full mirror not needed!
+#bench::bench_time({
+#  mc_mirror("osn/bio230014-bucket01/challenges/forecasts/parquet/", "forecasts/parquet/", overwrite = TRUE, remove=TRUE)
+#  mc_mirror("osn/bio230014-bucket01/challenges/forecasts/bundled-parquet/", "forecasts/bundled-parquet/", overwrite = TRUE, remove=TRUE)
+#})
 
-})
-
-
+remote_path <- "osn/bio230014-bucket01/challenges/forecasts/parquet/project_id=neon4cast/"
+remote_bundles <- "osn/bio230014-bucket01/challenges/forecasts/bundled-parquet/project_id=neon4cast/"
 ##OOOF, still fragile!
-durations <- mc_ls("forecasts/parquet/project_id=neon4cast/")
 
 by <- join_by(datetime, site_id, prediction, parameter, family,
               reference_datetime, pub_datetime, duration, model_id,
               project_id, variable)
+
 bench::bench_time({ # 18m w/ union, ~ 50 GB used at times
+
+  durations <- mc_ls(remote_path)
+  durations <- durations[grepl("duration", durations)]
+
   for (dur in durations) {
-    variables <- mc_ls(glue("forecasts/parquet/project_id=neon4cast/{dur}"))
+
+    variables <- mc_ls(glue(remote_path, "{dur}"))
+    variables <- variables[grepl("variable", variables)]
+
     for (var in variables) {
-      models <- mc_ls(glue("forecasts/parquet/project_id=neon4cast/{dur}{var}"))
+
+      models <- mc_ls(glue(remote_path, "{dur}{var}"))
+      models <- models[grepl("model_id", models)]
+
       for (model_id in models) {
-        path = glue("./forecasts/parquet/project_id=neon4cast/{dur}{var}{model_id}")
+        path = glue(remote_path, "{dur}{var}{model_id}")
         print(path)
-        readr::write_lines(path, "bundled.log", append=TRUE)
-        if(length(fs::dir_ls(path)) > 0) {
+
+        if(length(mc_ls(path)) > 0) {
+        if(any(grepl("[.]parquet$", mc_ls(path)))) {
 
           con = duckdbfs::cached_connection(tempfile())
+          duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
 
-          new <- open_dataset(path, conn = con) |>
+          s3_path <- gsub("^osn\\/", "s3://", path)
+          new <- open_dataset(s3_path, conn = con) |>
             filter( !is.na(model_id),
                     !is.na(parameter),
                     !is.na(prediction)) |>
             select(-any_of(c("date", "reference_date", "...1")))  # (date is a short version of datetime from partitioning, drop it)
 
 
-
-
-          bundles <- glue("forecasts/bundled-parquet/project_id=neon4cast/{dur}{var}{model_id}")
+          bundles <- glue(remote_bundles, "{dur}{var}{model_id}")
 
           ## if this model (dur/var/model) has submitted before, we need
           ## to 'append' any new forecasts via union() to retain bundle
 
 
-          if (fs::dir_exists(bundles)) {
+          if (is.character(mc_ls(bundles))) {
 
              # force eval and offload to disk
              new |> write_dataset("tmp_new.parquet")
 
              # do some clean-up of old as well
-             old <- open_dataset(bundles, conn = con) |>
+            s3_bundles <- gsub("^osn\\/", "s3://", bundles)
+
+             old <- open_dataset(s3_bundles, conn = con) |>
                filter( !is.na(model_id),
                        !is.na(parameter),
                        !is.na(prediction)) |>
@@ -97,75 +110,85 @@ bench::bench_time({ # 18m w/ union, ~ 50 GB used at times
                           options = list("PER_THREAD_OUTPUT false"))
 
           duckdbfs::close_connection(con); gc()
-        }
+        }}
       }
     }
   }
 })
 
+# only if we have new stuff!
+if(length(fs::dir_ls("new-forecasts/bundled-parquet/")) > 0) {
+
+  ## checks that we have no corruption
+  open_dataset("new-forecasts/bundled-parquet/") |> count()
+  open_dataset(fs::path("new-forecasts/", "bundled-parquet/")) |>
+    distinct(duration, variable, model_id)
+  open_dataset(fs::path("new-forecasts/", "bundled-parquet/")) |>
+    summarise(first_fc = min(reference_datetime), last_fc = max(reference_datetime),
+              first_prediction = min(datetime), last_prediction = max(datetime))
+
+}
+
+#s3_bundles <- gsub("^osn\\/", "s3://", remote_bundles)
+#open_dataset(s3_bundles) |>
+#  summarise(first_fc = min(reference_datetime), last_fc = max(reference_datetime),
+#            first_prediction = min(datetime), last_prediction = max(datetime))
 
 
-## checks that we have no corruption
-open_dataset("new-forecasts/bundled-parquet/") |> count()
-open_dataset(fs::path("new-forecasts/", "bundled-parquet/")) |>
-  distinct(duration, variable, model_id)
-open_dataset(fs::path("new-forecasts/", "bundled-parquet/")) |>
-  summarise(first_fc = min(reference_datetime), last_fc = max(reference_datetime),
-            first_prediction = min(datetime), last_prediction = max(datetime))
+mc_mirror("new-forecasts/bundled-parquet/project_id=neon4cast/", remote_bundles, overwrite =TRUE)
 
-
-## Now, new-bundled overwrites but does not remove bundled.  This appends to models with new forecasts, but doesn't remove models no longer receiving forecasts
-fs::dir_copy("new-forecasts/bundled-parquet/", "forecasts/bundled-parquet/", overwrite =TRUE)
+## online tests
+online <- open_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-parquet",
+                       s3_endpoint = "sdsc.osn.xsede.org",
+                       anonymous = TRUE)
 
 ## More checks
+online |> count()
+online |>  count(duration, variable, model_id)
 date_range <- open_dataset("forecasts/bundled-parquet/") |>
   summarise(first_fc = min(reference_datetime), last_fc = max(reference_datetime),
             first_prediction = min(datetime), last_prediction = max(datetime))
-#message(date_range)
 
 
 
-## Drop old forecasts so we don't keep rebundling them.  (keep last month for safety?)
-cutoff <- lubridate::dmonths(6)
-all_fc_files <- fs::dir_ls("forecasts/parquet/project_id=neon4cast", type="file", recurse = TRUE)
-dates <- all_fc_files |> stringr::str_extract("reference_date=(\\d{4}-\\d{2}-\\d{2})/", 1)  |> as.Date()
-drop <- dates < Sys.Date() - cutoff
-drop_paths <- all_fc_files[drop]
 
-drop_paths |> fs::file_delete()
+## Drop old forecasts so we don't keep rebundling them.  (keep last n-months for safety?)
 
-bench::bench_time({ # 12.1m
-  mc_mirror("forecasts/bundled-parquet", "osn/bio230014-bucket01/challenges/forecasts/bundled-parquet",
-            overwrite = TRUE, remove = TRUE)
-})
-
-
-
-## Instead of remove, we could move to archive.  Only once we have successfully updated the bundles!
-## really really slow
-s3_drop_paths <- paste0("osn/bio230014-bucket01/challenges/", gsub("^\\./", "", drop_paths))
-
-drop_f <- function(path) {
-  if(is.character(mc_ls(path)))
-    mc_mv(path, gsub("forecasts\\/parquet", "forecasts/archive-parquet",  path))
-  else
-    invisible(NULL)
+archive_older <- function(remote_path = "osn/bio230014-bucket01/challenges/forecasts/parquet/project_id=neon4cast",
+                          keep_months = 6) {
+  cutoff <- lubridate::dmonths(keep_months)
+  all_fc_files <- paste0(remote_path, "/", mc_ls(remote_path, recursive = TRUE))
+  dates <- all_fc_files |>
+    stringr::str_extract("reference_date=(\\d{4}-\\d{2}-\\d{2})", 1)  |>
+    as.Date()
+  drop <- dates < Sys.Date() - cutoff
+  drop_paths <- all_fc_files[drop] |> na.omit()
+  return(drop_paths)
 }
-parallel::mclapply(s3_drop_paths, drop_f, mc.cores = parallel::detectCores)
 
 
-## no need to mirror unchanged unbundled forecasts.
+drop_f <- function(path,
+                   from_pattern = "forecasts\\/parquet",
+                   dest_pattern = "forecasts/archive-parquet") {
+  if( grepl(".parquet$", path) ){
+    mc_mv(path, gsub(from_pattern, dest_pattern, path))
+    mc_rm(dirname(path), recursive = TRUE)
+  } else {
+    mc_rm(path)
+  }
+  invisible("success")
+}
+
+drop_paths <- archive_older(remote_path, 6)
+parallel::mclapply(drop_paths, drop_f, mc.cores = 6)
+
+
+# shouldn't be drop paths left:
+print(length(archive_older(remote_path, 6)) == 0)
 
 
 ## We are done.
 
 
-## online tests
-
-online <- open_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-parquet",
-                       s3_endpoint = "sdsc.osn.xsede.org",
-                       anonymous = TRUE)
-online |> count()
-online |>  count(duration, variable, model_id)
 
 
--- scoring/bundled-summaries.R
+++ scoring/bundled-summaries.R
@@ -10,52 +10,33 @@ library(fs)
 
 install_mc()
 mc_alias_set("osn", "sdsc.osn.xsede.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
+duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
 
 
-# make sure new-forecasts location exists and is empty.
-fs::dir_create("new-forecasts"); fs::dir_delete("new-forecasts")
-fs::dir_create("forecasts/bundled-summaries")
-fs::dir_create("new-forecasts/bundled-summaries")
+s3_summaries <- "s3://bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/"
+existing_bundles <- "s3://bio230014-bucket01/challenges/forecasts/bundled-summaries/project_id=neon4cast/"
 
-# Sync to local, fastest way to access all the bytes.
-bench::bench_time({
-  # mirror everything(!) crazy
-  # Could focus on summaries here
-  mc_mirror("osn/bio230014-bucket01/challenges/forecasts/summaries/", "forecasts/summaries/", overwrite = TRUE, remove = TRUE)
-  mc_mirror("osn/bio230014-bucket01/challenges/forecasts/bundled-summaries/", "forecasts/bundled-summaries/", overwrite = TRUE, remove = TRUE)
 
-})
+## how many currently bundled rows of data:
+open_dataset(existing_bundles) |>   count()
 
 
 grouping <- c("model_id", "reference_datetime", "site_id",
               "datetime", "family", "variable", "duration", "project_id")
-
-
-## Tidy the current bundled summaries
-#bundled_summmaries <- duckdbfs::open_dataset("forecasts/bundled-summaries/")
-#bundled_summmaries |>
-#  distinct() |>
-#  write_dataset("new-forecasts/bundled-summaries/project_id=neon4cast",
-#                partitioning = c("duration", 'variable', "model_id"),
-#                options = list("PER_THREAD_OUTPUT false"))
-#mc_mirror("new-forecasts/bundled-summaries/", "forecasts/bundled-summaries/", overwrite = TRUE, remove = TRUE)
-#mc_mirror("forecasts/bundled-summaries/", "osn/bio230014-bucket01/challenges/forecasts/bundled-summaries/", overwrite = TRUE, remove = TRUE)
-
-
-
 bench::bench_time({
-  bundled_summaries <- open_dataset("./forecasts/bundled-summaries/project_id=neon4cast")
-  new_summaries <- open_dataset("./forecasts/summaries/project_id=neon4cast/")
+  bundled_summaries <- open_dataset(existing_bundles)
+  new_summaries <- open_dataset(s3_summaries)
   union(bundled_summaries, new_summaries) |>
     filter(!is.na(model_id)) |>  ## model_id CANNOT BE NA!
     write_dataset("tmp.parquet")
 
+# because we have written to tmp, we can stream back up to bundled-summaries
 # Ensures partitions are written as a single shard
   open_dataset("tmp.parquet") |>
       group_by(across(any_of(grouping))) |>
       slice_max(pub_datetime) |>
       distinct() |>
-      write_dataset("new-forecasts/bundled-summaries/project_id=neon4cast",
+      write_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-summaries/project_id=neon4cast",
                     partitioning = c("duration", 'variable', "model_id"),
                     options = list("PER_THREAD_OUTPUT false"))
 
@@ -63,30 +44,30 @@ bench::bench_time({
 
 
 
-# check that we have no corruption
-n_bundled <- open_dataset(fs::path("new-forecasts", "bundled-summaries/")) |> count() |> collect()
-n_groups <- open_dataset(fs::path("new-forecasts", "bundled-summaries/")) |>
-  distinct(duration, variable, model_id) |> count() |> collect()
+archive_older <- function(remote_path = "osn/bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/",
+                          keep_months = 6) {
+  cutoff <- lubridate::dmonths(keep_months)
+  all_fc_files <- fs::path(remote_path, mc_ls(remote_path, recursive = TRUE))
+  dates <- all_fc_files |>
+    stringr::str_extract("reference_date=(\\d{4}-\\d{2}-\\d{2})", 1)  |>
+    as.Date()
+  drop <- dates < Sys.Date() - cutoff
+  drop_paths <- all_fc_files[drop] |> na.omit()
+  return(fs::path(drop_paths))
+}
 
+drop_f <- function(path,
+                   from_pattern = "forecasts\\/summaries",
+                   dest_pattern = "forecasts/archive-summaries") {
+  if( grepl(".parquet$", path) ){
+    mc_mv(path, gsub(from_pattern, dest_pattern, path))
+    mc_rm(dirname(path), recursive = TRUE)
+  } else {
+    mc_rm(path)
+  }
+  invisible("success")
+}
 
-# PURGE all but last 2 months from un-bundled
-all_fc_files <- fs::dir_ls("forecasts/summaries/project_id=neon4cast", type="file", recurse = TRUE)
-dates <- all_fc_files |> stringr::str_extract("reference_date=(\\d{4}-\\d{2}-\\d{2})/", 1)  |> lubridate::as_date()
-drop <- dates < (Sys.Date() - lubridate::dmonths(2))
-all_fc_files[drop] |> fs::file_delete()
+drop_paths <- archive_older("osn/bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/", 1)
+parallel::mclapply(drop_paths, drop_f, from_pattern = "forecasts\\/summaries", dest_pattern =  "forecasts/archive-summaries", mc.cores = 12)
 
-
-fs::dir_delete("forecasts/bundled-summaries/")
-fs::dir_copy("new-forecasts/bundled-summaries/", "forecasts/bundled-summaries/", overwrite =TRUE)
-
-
-
-## upload new bundles, overwriting old ones.
-bench::bench_time({
-  mc_mirror("forecasts/summaries",
-            "osn/bio230014-bucket01/challenges/forecasts/summaries",
-            remove=TRUE)
-  mc_mirror("forecasts/bundled-summaries",
-            "osn/bio230014-bucket01/challenges/forecasts/bundled-summaries",
-            overwrite=TRUE, remove=TRUE)
-})

9517ef1bfb1020b8c853cd35dcc97b43ce7be89f
Carl Boettiger <cboettig@gmail.com>
2025-07-21 18:03:25 +0000

    append yay!

--- scoring/bundled-forecasts.R
+++ scoring/bundled-forecasts.R
@@ -1,9 +1,6 @@
 
 remotes::install_github("cboettig/duckdbfs", upgrade=FALSE)
 
-
-
-
 library(tidyverse)
 library(duckdbfs)
 library(minioclient)
@@ -11,21 +8,19 @@ library(bench)
 library(glue)
 library(fs)
 
+
+
 install_mc()
 mc_alias_set("osn", "sdsc.osn.xsede.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
 # mc_alias_set("nrp", "s3-west.nrp-nautilus.io", Sys.getenv("EFI_NRP_KEY"), Sys.getenv("EFI_NRP_SECRET"))
 
 duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
 
-
+# bundled count at start
 open_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-parquet",
              s3_endpoint = "sdsc.osn.xsede.org",
              anonymous = TRUE) |>
   count()
-
-# make sure new-forecasts location exists and is empty.
-fs::dir_create("new-forecasts"); fs::dir_delete("new-forecasts")
-fs::dir_create("new-forecasts/bundled-parquet")
 
 
 remote_path <- "osn/bio230014-bucket01/challenges/forecasts/parquet/project_id=neon4cast/"
@@ -45,42 +40,35 @@ bundle_me <- function(path) {
   print(path)
   con = duckdbfs::cached_connection(tempfile())
   duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
+  bundled_path <- path |> str_replace(fixed("forecasts/parquet"), "forecasts/bundled-parquet")
 
-  new <- open_dataset(path, conn = con) |>
-  filter( !is.na(model_id),
-          !is.na(parameter),
-          !is.na(prediction)) |>
-  select(-any_of(c("date", "reference_date", "...1")))  # (date is a short version of datetime from partitioning, drop it)
+  open_dataset(path, conn = con) |>
+    filter( !is.na(model_id),
+            !is.na(parameter),
+            !is.na(prediction)) |>
+    select(-any_of(c("date", "reference_date", "...1"))) |>
+    write_dataset("tmp_new.parquet")
 
-  new |> write_dataset("tmp_new.parquet")
-
-  bundled_path <- path |>
-    str_replace(fixed("forecasts/parquet"), "forecasts/bundled-parquet")
-
-
-  old <-
-     open_dataset(bundled_path, conn = con) |>
-     filter( !is.na(model_id),
-             !is.na(parameter),
-             !is.na(prediction)) |>
-     select(-any_of(c("date", "reference_date", "...1"))) |>
+  # special filters should not be needed on bundled copy
+  open_dataset(bundled_path, conn = con) |>
      write_dataset("tmp_old.parquet")
 
-  by <- join_by(datetime, site_id, prediction, parameter, family,
-            reference_datetime, pub_datetime, duration, model_id,
-            project_id, variable)
 
   # these are both local, so we can stream back.
   new <- open_dataset("tmp_new.parquet")
-  old <- open_dataset("tmp_old.parquet") |>
-        anti_join(new, by = by)
+  old <- open_dataset("tmp_old.parquet")
+
+## We can just "append", we no longer face duplicates:
+# by <- join_by(datetime, site_id, prediction, parameter, family, reference_datetime, pub_datetime, duration, model_id, project_id, variable)
+#  filtered_n <- old |> anti_join(new, by = by) |> count() |> pull(n) # is this the bottleneck?
+#  previous_n <- open_dataset("tmp_old.parquet") |> count() |> pull(n)
+#  stopifnot(previous_n - filtered_n == 0)
 
   union_all(old, new) |>
             write_dataset(bundled_path,
                           options = list("PER_THREAD_OUTPUT false"))
 
   #We should now archive anything we have bundled:
-
   mc_path <- path |> str_replace(fixed("s3://"), "osn/")
   dest_path <- mc_path |>
     str_replace(fixed("forecasts/parquet"), "forecasts/archive-parquet")
@@ -90,11 +78,14 @@ bundle_me <- function(path) {
   mc_rm(mc_path, recursive = TRUE)
 
   duckdbfs::close_connection(con); gc()
+
+  invisible(0)
 }
 
+try_bundles <- purrr::possibly(bundle_me)
 
 bench::bench_time({
-lapply(model_paths, bundle_me)
+out <- purrr::map(model_paths, try_bundles)
 })
 
 
@@ -103,3 +94,5 @@ lapply(model_paths, bundle_me)
 
 
 
+# should we slice_max(pub_time) to ensure only most recent pub_time if duplicates submitted?
+# grouping <- c("model_id", "reference_datetime", "site_id", "datetime", "family", "variable", "duration", "project_id")
--- scoring/bundled-summaries.R
+++ scoring/bundled-summaries.R
@@ -1,7 +1,7 @@
+
 remotes::install_github("cboettig/duckdbfs", upgrade=FALSE)
 
-
-library(dplyr)
+library(tidyverse)
 library(duckdbfs)
 library(minioclient)
 library(bench)
@@ -10,66 +10,72 @@ library(fs)
 
 install_mc()
 mc_alias_set("osn", "sdsc.osn.xsede.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
+# mc_alias_set("nrp", "s3-west.nrp-nautilus.io", Sys.getenv("EFI_NRP_KEY"), Sys.getenv("EFI_NRP_SECRET"))
+
 duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
 
-
-s3_summaries <- "s3://bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/"
-existing_bundles <- "s3://bio230014-bucket01/challenges/forecasts/bundled-summaries/project_id=neon4cast/"
-
-
-## how many currently bundled rows of data:
-open_dataset(existing_bundles) |>   count()
+# bundled count at start
+open_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-summaries",
+             s3_endpoint = "sdsc.osn.xsede.org",
+             anonymous = TRUE) |>
+  count()
 
 
-grouping <- c("model_id", "reference_datetime", "site_id",
-              "datetime", "family", "variable", "duration", "project_id")
+remote_path <- "osn/bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/"
+contents <- mc_ls(remote_path, recursive = TRUE, details = TRUE)
+data_paths <- contents |> filter(!is_folder) |> pull(path)
+
+# model paths are paths with at least one reference_datetime containing data files
+model_paths <-
+  data_paths |>
+  str_replace_all("reference_date=\\d{4}-\\d{2}-\\d{2}/.*", "") |>
+  str_replace("^osn\\/", "s3://") |>
+  unique()
+
+
+bundle_me <- function(path) {
+
+  print(path)
+  con = duckdbfs::cached_connection(tempfile())
+  duckdb_secrets(endpoint = "sdsc.osn.xsede.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230014-bucket01")
+
+
+  bundled_path <- path |>
+    str_replace(fixed("forecasts/summaries"), "forecasts/bundled-summaries")
+
+  open_dataset(path, conn = con) |> write_dataset("tmp_new.parquet")
+  open_dataset(bundled_path, conn = con) |> write_dataset("tmp_old.parquet")
+
+  # these are both local, so we can stream back.
+  new <- open_dataset("tmp_new.parquet")
+  old <- open_dataset("tmp_old.parquet")
+
+  union_all(old, new) |>
+    write_dataset(bundled_path,
+                  options = list("PER_THREAD_OUTPUT false"))
+
+  #We should now archive anything we have bundled:
+
+  mc_path <- path |> str_replace(fixed("s3://"), "osn/")
+  dest_path <- mc_path |>
+    str_replace(fixed("forecasts/summaries"), "forecasts/archive-summaries")
+  mc_mv(mc_path, dest_path, recursive = TRUE)
+  # clears up empty folders
+  mc_rm(mc_path, recursive = TRUE)
+
+  duckdbfs::close_connection(con); gc()
+  invisible(0)
+}
+
+try_bundles <- purrr::possibly(bundle_me)
+
 bench::bench_time({
-  bundled_summaries <- open_dataset(existing_bundles)
-  new_summaries <- open_dataset(s3_summaries)
-  union(bundled_summaries, new_summaries) |>
-    filter(!is.na(model_id)) |>  ## model_id CANNOT BE NA!
-    write_dataset("tmp.parquet")
-
-# because we have written to tmp, we can stream back up to bundled-summaries
-# Ensures partitions are written as a single shard
-  open_dataset("tmp.parquet") |>
-      group_by(across(any_of(grouping))) |>
-      slice_max(pub_datetime) |>
-      distinct() |>
-      write_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-summaries/project_id=neon4cast",
-                    partitioning = c("duration", 'variable', "model_id"),
-                    options = list("PER_THREAD_OUTPUT false"))
-
+  out <- purrr::map(model_paths, try_bundles)
 })
 
 
-
-archive_older <- function(remote_path = "osn/bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/",
-                          keep_months = 0.25) {
-  cutoff <- lubridate::dmonths(keep_months)
-  contents <- mc_ls(remote_path, recursive = TRUE, details = TRUE)
-  all_fc_files <- contents |> filter(!is_folder) |> pull(path)
-
-  dates <- all_fc_files |>
-    stringr::str_extract("reference_date=(\\d{4}-\\d{2}-\\d{2})", 1)  |>
-    as.Date()
-  drop <- dates < Sys.Date() - cutoff
-  drop_paths <- all_fc_files[drop] |> na.omit()
-  return(fs::path(drop_paths))
-}
-
-drop_f <- function(path,
-                   from_pattern = "forecasts\\/summaries",
-                   dest_pattern = "forecasts/archive-summaries") {
-  if( grepl(".parquet$", path) ){
-    mc_mv(path, gsub(from_pattern, dest_pattern, path))
-    mc_rm(dirname(path), recursive = TRUE)
-  } else {
-    mc_rm(path)
-  }
-  invisible("success")
-}
-
-drop_paths <- archive_older("osn/bio230014-bucket01/challenges/forecasts/summaries/project_id=neon4cast/")
-out <- parallel::mclapply(drop_paths, drop_f, from_pattern = "forecasts\\/summaries", dest_pattern =  "forecasts/archive-summaries", mc.cores = 12)
-
+# bundled count at start
+open_dataset("s3://bio230014-bucket01/challenges/forecasts/bundled-summaries",
+             s3_endpoint = "sdsc.osn.xsede.org",
+             anonymous = TRUE) |>
+  count()

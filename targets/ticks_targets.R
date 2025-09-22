62f4607c34ea99fb0e772bd6c09858598a87705f
rqthomas <rqthomas@vt.edu>
2025-09-22 11:37:37 -0400

    updating target generation

--- targets/ticks_targets.R
+++ targets/ticks_targets.R
@@ -2,10 +2,11 @@ library(tidyverse) # for data wrangling and piping (dplyr probably ok)
 library(lubridate) # for finding year from dates
 library(neonstore)
 library(minioclient)
+library(neonUtilities)
 
 install_mc()
 mc_alias_set("osn", "sdsc.osn.xsede.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
-mc_mirror("osn/bio230014-bucket01/ticks-data/",  path.expand("~/ticks-data/"))
+#mc_mirror("osn/bio230014-bucket01/ticks-data/",  path.expand("~/ticks-data/"))
 
 # select target species and life stage
 target_species <- c("Amblyomma americanum") # NEON species name
@@ -18,55 +19,59 @@ target_sites <- sites_df %>% pull(field_site_id)
 
 source("targets/R/resolve_taxonomy.R")
 
-pass <- TRUE
-iter <- 0
-while(pass & iter < 10){
-  iter <- iter + 1
+# pass <- TRUE
+# iter <- 0
+# while(pass & iter < 10){
+#   iter <- iter + 1
+#
+#   df <-  neonstore:::neon_data(product = "DP1.10093.001",
+#                                #start_date = paste0(curr_year, "-01-01"),
+#                                #end_date = paste0(curr_year, "-12-31"),
+#                                site = target_sites,
+#                                type="expanded")
+#
+#   if(file.exists(path.expand("~/ticks-data/DP1.10093.001.csv"))){
+#     full_df_old <- read_csv(path.expand("~/ticks-data/DP1.10093.001.csv"), show_col_types = FALSE)
+#   }else{
+#     full_df_old <- NULL
+#   }
+#
+#   full_df <- bind_rows(full_df_old, df) %>%
+#     distinct()
+#
+#   print(nrow(full_df))
+#   print(nrow(full_df_old))
+#   pass <- nrow(full_df) != nrow(full_df_old)
+#
+#   write_csv(full_df, path.expand("~/ticks-data/DP1.10093.001.csv"))
+# }
 
-  df <-  neonstore:::neon_data(product = "DP1.10093.001",
-                               #start_date = paste0(curr_year, "-01-01"),
-                               #end_date = paste0(curr_year, "-12-31"),
-                               site = target_sites,
-                               type="expanded")
 
-  if(file.exists(path.expand("~/ticks-data/DP1.10093.001.csv"))){
-    full_df_old <- read_csv(path.expand("~/ticks-data/DP1.10093.001.csv"), show_col_types = FALSE)
-  }else{
-    full_df_old <- NULL
-  }
-
-  full_df <- bind_rows(full_df_old, df) %>%
-    distinct()
-
-  print(nrow(full_df))
-  print(nrow(full_df_old))
-  pass <- nrow(full_df) != nrow(full_df_old)
-
-  write_csv(full_df, path.expand("~/ticks-data/DP1.10093.001.csv"))
-}
-
-fielddata_urls <- full_df |>
-  dplyr::filter(grepl("tck_fielddata", name)) |>
-  pull(url)
-
-taxonomyProcessed_urls <- full_df |>
-  dplyr::filter(grepl("tck_taxonomyProcessed", name)) |>
-  pull(url)
-
-tick_field_raw <- duckdbfs::open_dataset(fielddata_urls, format="csv") |>
+tick_field_raw <- datasetQuery(dpID="DP1.10093.001",
+                        package="expanded",
+                        tabl="tck_fielddata",
+                        release="current",
+                        include.provisional = TRUE,
+                        token=Sys.getenv("NEON_TOKEN")) |>
   select(totalSampledArea, collectDate, namedLocation, nlcdClass, siteID) |>
   collect()
 
-tick_taxon_raw <- duckdbfs::open_dataset(taxonomyProcessed_urls, format="csv") |>
+tick_taxon_raw <- datasetQuery(dpID="DP1.10093.001",
+                               package="expanded",
+                               tabl="tck_taxonomyProcessed",
+                               release="current",
+                               include.provisional = TRUE,
+                               token=Sys.getenv("NEON_TOKEN")) |>
   select(sampleCondition, sexOrAge, collectDate, namedLocation,
          acceptedTaxonID, individualCount, scientificName, taxonRank) |>
   collect()
+
 
 # there are lots of reasons why sampling didn't occur (logistics, too wet, too cold, etc.)
 # so, keep records when sampling occurred
 tick_field <- tick_field_raw %>%
   filter(totalSampledArea > 0) %>%
-  mutate(collectDate = lubridate::fast_strptime(collectDate, "%Y-%m-%dT%H:%MZ"),
+  mutate(collectDate = lubridate::as_datetime(collectDate),
          time = floor_date(collectDate, unit = "day")) %>%
   unite(namedLocation, time, col = "occasionID", sep = "_")
 
@@ -76,7 +81,7 @@ tick_taxon_wide <- tick_taxon_raw %>%
   mutate(sexOrAge = if_else(sexOrAge == "Female" | sexOrAge == "Male",
                             "Adult",     # convert to Adult
                             sexOrAge),
-         collectDate = lubridate::fast_strptime(collectDate, "%Y-%m-%dT%H:%MZ"),
+         collectDate = lubridate::as_datetime(collectDate),
          time = floor_date(collectDate, unit = "day")) %>%
   unite(namedLocation, time, col = "occasionID", sep = "_") %>%
   pivot_wider(id_cols = occasionID, # make wide by species and life stage
@@ -139,9 +144,7 @@ tick_targets <- tick_standard %>%
   mutate(variable = ifelse(variable == "Amblyomma americanum", "amblyomma_americanum", "ixodes_scapularis")) |>
   select(time, site_id, variable, observation, iso_week)
 
-ggplot(tick_targets, aes(x = time, y = observation, color = variable)) +
-  geom_point() +
-  facet_wrap(~site_id, scale = "free")
+
 
 tick_targets <- tick_targets |>
   rename(datetime = time)
@@ -152,11 +155,16 @@ tick_targets2 <- tick_targets |>
          project_id = "neon4cast") |>
   select(project_id, site_id, datetime, duration, variable, observation)
 
+
+ggplot(tick_targets2, aes(x = datetime, y = observation, color = variable)) +
+  geom_point() +
+  facet_wrap(~site_id, scale = "free")
+
 write_csv(tick_targets2, "ticks-targets.csv.gz")
 message("Writing targets to S3")
 mc_cp("ticks-targets.csv.gz", "osn/bio230014-bucket01/challenges/targets/project_id=neon4cast/duration=P1W/")
-message("Writing data catalog to S3")
-mc_mirror(path.expand("~/ticks-data"), "osn/bio230014-bucket01/ticks-data/", overwrite = TRUE, remove = TRUE)
+#message("Writing data catalog to S3")
+#mc_mirror(path.expand("~/ticks-data"), "osn/bio230014-bucket01/ticks-data/", overwrite = TRUE, remove = TRUE)
 
 RCurl::getURL("https://hc-ping.com/09c7ab10-eb4e-40ef-a029-7a4addc3295b")
 

683741946349977bca8534b5902d4ba608a0c756
Katherine Rein <157232885+katherineanne21@users.noreply.github.com>
2025-09-26 10:33:36 -0400

    Fix Syntax

--- S3_Bucket_ReadWrite.R
+++ S3_Bucket_ReadWrite.R
@@ -15,14 +15,14 @@ OUR_ENDPOINT_OVERRIDE = 'https://minio-s3.apps.shift.nerc.mghpcc.org'
 if (Sys.getenv("GITHUB_ACTIONS") == "true") {
   # Set Up S3 Connection (Git Hub)
   # Keys saved in Settings -> Secrets + Variables -> Actions -> Repository secrets
-  s3_read <- arrow::s3_bucket((OUR_LINK_read,
+  s3_read <- arrow::s3_bucket(OUR_LINK_read,
                                endpoint_override = OUR_ENDPOINT_OVERRIDE,
                                access_key = Sys.getenv("OSN_KEY"),
                                secret_key = Sys.getenv("OSN_SECRET"),
                                scheme = "https"
   )
   
-  s3_write <- arrow::s3_bucket((OUR_LINK_write,
+  s3_write <- arrow::s3_bucket(OUR_LINK_write,
                                endpoint_override = OUR_ENDPOINT_OVERRIDE,
                                access_key = Sys.getenv("OSN_KEY"),
                                secret_key = Sys.getenv("OSN_SECRET"),

1337e9aac134a0b39065542c1827c15f5b350a3e
Cami Webb <cwebb16@bu.edu>
2026-03-02 18:12:25 -0500

    Update update_stage3.R

--- drivers/update_stage3.R
+++ drivers/update_stage3.R
@@ -1,66 +1,75 @@
 library(arrow)
-
+library(dplyr)
 source("https://raw.githubusercontent.com/eco4cast/neon4cast/main/R/to_hourly.R")
-
 print(sessioninfo::package_info())
 
-site_list <- readr::read_csv(paste0("https://github.com/eco4cast/",
-                                    "neon4cast-noaa-download/",
-                                    "raw/master/noaa_download_site_list.csv"),
-                             show_col_types = FALSE) |> dplyr::pull(site_id)
+# Read sites from bu4cast bucket
+s3_read <- arrow::s3_bucket(
+  "bu4cast-ci-read",
+  endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
+  access_key = Sys.getenv("OSN_KEY"),
+  secret_key = Sys.getenv("OSN_SECRET"),
+  scheme = "https"
+)
 
-#future::plan("future::multisession", workers = 8)
+site_list <- arrow::read_csv_arrow(
+  s3_read$path("challenges/targets/project_id=bu4cast/field_sites.csv")
+) %>%
+  as.data.frame() %>%
+  dplyr::pull(field_site_id)
 
-purrr::map(site_list, function(curr_site_id){
+message("Sites loaded: ", length(site_list))
 
+purrr::map(site_list, function(curr_site_id) {
   print(curr_site_id)
 
-  s3 <- arrow::s3_bucket("bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/stage3",
-                         endpoint_override = "sdsc.osn.xsede.org",
-                         access_key= Sys.getenv("OSN_KEY"),
-                         secret_key= Sys.getenv("OSN_SECRET"))
+  s3 <- arrow::s3_bucket(
+    "bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/stage3",
+    endpoint_override = "sdsc.osn.xsede.org",
+    access_key = Sys.getenv("OSN_KEY"),
+    secret_key = Sys.getenv("OSN_SECRET")
+  )
 
-  stage3_df <- arrow::open_dataset(s3) |>
-    dplyr::filter(site_id == curr_site_id) |>
+  stage3_df <- arrow::open_dataset(s3) %>%
+    dplyr::filter(site_id == curr_site_id) %>%
     dplyr::collect()
 
-  max_date <- stage3_df |>
-    dplyr::summarise(max = as.character(lubridate::as_date(max(datetime)))) |>
+  max_date <- stage3_df %>%
+    dplyr::summarise(max = as.character(lubridate::as_date(max(datetime)))) %>%
     dplyr::pull(max)
 
-  s3_pseudo <- arrow::s3_bucket("bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/pseudo",
-                                endpoint_override = "sdsc.osn.xsede.org",
-                                access_key= Sys.getenv("OSN_KEY"),
-                                secret_key= Sys.getenv("OSN_SECRET"))
-
-  vars <- names(stage3_df)
+  s3_pseudo <- arrow::s3_bucket(
+    "bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/pseudo",
+    endpoint_override = "sdsc.osn.xsede.org",
+    access_key = Sys.getenv("OSN_KEY"),
+    secret_key = Sys.getenv("OSN_SECRET")
+  )
 
   cut_off <- as.character(lubridate::as_date(max_date) - lubridate::days(3))
 
-  df <- arrow::open_dataset(s3_pseudo) |>
-    dplyr::filter(variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF")) |>
+  df <- arrow::open_dataset(s3_pseudo) %>%
+    dplyr::filter(variable %in% c("PRES", "TMP", "RH", "UGRD", "VGRD", "APCP", "DSWRF", "DLWRF")) %>%
     dplyr::filter(site_id == curr_site_id,
-                  reference_datetime >= cut_off) |>
-    dplyr::collect() |>
-    dplyr::mutate(date = lubridate::as_date(reference_datetime),
-                  new_datetime = date + lubridate::hours(as.numeric(horizon)) + lubridate::hours(as.numeric(cycle)),
-                  datetime = ifelse(datetime != new_datetime, new_datetime, datetime),
-                  datetime = lubridate::as_datetime(datetime)) |>
+                  reference_datetime >= cut_off) %>%
+    dplyr::collect() %>%
+    dplyr::mutate(
+      date         = lubridate::as_date(reference_datetime),
+      new_datetime = date + lubridate::hours(as.numeric(horizon)) + lubridate::hours(as.numeric(cycle)),
+      datetime     = ifelse(datetime != new_datetime, new_datetime, datetime),
+      datetime     = lubridate::as_datetime(datetime)
+    ) %>%
     dplyr::select(-date, -new_datetime)
 
-  if(nrow(df) > 0){
+  if (nrow(df) > 0) {
+    df2 <- df %>%
+      to_hourly(use_solar_geom = TRUE, psuedo = TRUE) %>%
+      dplyr::mutate(ensemble = as.numeric(stringr::str_sub(ensemble, start = 4, end = 5))) %>%
+      dplyr::rename(parameter = ensemble)
 
-    df2 <- df |>
-      to_hourly(use_solar_geom = TRUE, psuedo = TRUE) |>
-    dplyr::mutate(ensemble = as.numeric(stringr::str_sub(ensemble, start = 4, end = 5))) |>
-    dplyr::rename(parameter = ensemble)
-
-    stage3_df_update <- stage3_df |>
-      dplyr::filter(datetime < min(df2$datetime))
-
-    df2 |>
-      dplyr::bind_rows(stage3_df_update) |>
-      dplyr::arrange(variable, datetime, parameter) |>
+    stage3_df %>%
+      dplyr::filter(datetime < min(df2$datetime)) %>%
+      dplyr::bind_rows(df2) %>%
+      dplyr::arrange(variable, datetime, parameter) %>%
       arrow::write_dataset(path = s3, partitioning = "site_id")
   }
 })

1b44b648fdc760bebcbdc989b8f191062d9eb10e
Cami Webb <cwebb16@bu.edu>
2026-03-02 17:58:24 -0500

    Update download_psuedo.R

--- drivers/download_psuedo.R
+++ drivers/download_psuedo.R
@@ -2,33 +2,37 @@
 library(gdalcubes)
 library(gefs4cast)
 library(arrow)
-
+library(dplyr)
 print(sessioninfo::package_info())
+gdalcubes::gdalcubes_options(parallel = 20)
 
-gdalcubes::gdalcubes_options(parallel=20)
-#gdalcubes::gdalcubes_options(parallel=TRUE)
+# Read sites from bu4cast bucket
+s3_read <- arrow::s3_bucket(
+  "bu4cast-ci-read",
+  endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
+  access_key = Sys.getenv("OSN_KEY"),
+  secret_key = Sys.getenv("OSN_SECRET"),
+  scheme = "https"
+)
 
-sites <-
-  dplyr::bind_rows(
-    readr::read_csv(paste0("https://github.com/eco4cast/",
-                           "neon4cast-noaa-download/",
-                           "raw/master/noaa_download_site_list.csv"),
-                    col_select = c("site_id", "latitude", "longitude"))#,
-    #readr::read_csv(paste0("https://github.com/eco4cast/neon4cast-targets/",
-    #                       "raw/main/tern_field_site_metadata.csv"),
-    #                col_select = c("site_id", "latitude", "longitude"))
+sites <- arrow::read_csv_arrow(
+  s3_read$path("challenges/targets/project_id=bu4cast/field_sites.csv")
+) %>%
+  as.data.frame() %>%
+  transmute(
+    site_id   = as.character(field_site_id),
+    latitude  = as.numeric(latitude),
+    longitude = as.numeric(longitude)
   )
 
+message("Sites loaded: ", nrow(sites))
 
-
-Sys.setenv("GEFS_VERSION"="v12")
-dates <- seq(as.Date("2020-09-24"), Sys.Date()-1, by=1)
-dates_pseudo <- seq(as.Date("2020-09-24"), Sys.Date(), by=1)
+Sys.setenv("GEFS_VERSION" = "v12")
+dates        <- seq(as.Date("2020-09-24"), Sys.Date() - 1, by = 1)
+dates_pseudo <- seq(as.Date("2020-09-24"), Sys.Date(),     by = 1)
 
 message("GEFS v12 pseudo")
-##bench::bench_time({ #32xlarge
-  s3 <- gefs_s3_dir("pseudo")
-  have_dates <- gsub("reference_datetime=", "", s3$ls())
-  missing_dates <- dates_pseudo[!(as.character(dates_pseudo) %in% have_dates)]
-  gefs4cast:::gefs_pseudo_measures(missing_dates,  path = s3, sites = sites)
-##})
+s3 <- gefs_s3_dir("pseudo")
+have_dates    <- gsub("reference_datetime=", "", s3$ls())
+missing_dates <- dates_pseudo[!(as.character(dates_pseudo) %in% have_dates)]
+gefs4cast:::gefs_pseudo_measures(missing_dates, path = s3, sites = sites)

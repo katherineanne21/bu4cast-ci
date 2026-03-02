5fb178bfec65f4756d4d089847cdd65db4db60aa
Cami Webb <cwebb16@bu.edu>
2026-03-02 18:20:48 -0500

    Update download_psuedo.R

--- drivers/download_psuedo.R
+++ drivers/download_psuedo.R
@@ -6,7 +6,7 @@ library(dplyr)
 print(sessioninfo::package_info())
 gdalcubes::gdalcubes_options(parallel = 20)
 
-# Read sites from bu4cast bucket
+# Read bucket for sites and driver storage
 s3_read <- arrow::s3_bucket(
   "bu4cast-ci-read",
   endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
@@ -32,7 +32,7 @@ dates        <- seq(as.Date("2020-09-24"), Sys.Date() - 1, by = 1)
 dates_pseudo <- seq(as.Date("2020-09-24"), Sys.Date(),     by = 1)
 
 message("GEFS v12 pseudo")
-s3 <- gefs_s3_dir("pseudo")
+s3 <- s3_read$path("challenges/targets/project_id=bu4cast/drivers/pseudo")
 have_dates    <- gsub("reference_datetime=", "", s3$ls())
 missing_dates <- dates_pseudo[!(as.character(dates_pseudo) %in% have_dates)]
 gefs4cast:::gefs_pseudo_measures(missing_dates, path = s3, sites = sites)

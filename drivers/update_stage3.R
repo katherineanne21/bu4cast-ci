3c494ed511628fc146d1dc8224f0c33e177fb84f
Cami Webb <cwebb16@bu.edu>
2026-03-04 15:22:35 -0500

    Update update_stage3.R

--- drivers/update_stage3.R
+++ drivers/update_stage3.R
@@ -13,7 +13,7 @@ s3_read <- arrow::s3_bucket(
 )
 
 site_list <- arrow::read_csv_arrow(
-  s3_read$path("challenges/targets/project_id=bu4cast/field_sites.csv")
+  s3_read$path("challenges/project_id=bu4cast/metadata/field_sites.csv")
 ) %>%
   as.data.frame() %>%
   dplyr::pull(field_site_id)
@@ -23,8 +23,8 @@ message("Sites loaded: ", length(site_list))
 purrr::map(site_list, function(curr_site_id) {
   print(curr_site_id)
 
-  s3_stage3 <- s3_read$path("challenges/targets/project_id=bu4cast/drivers/stage3")
-  s3_pseudo  <- s3_read$path("challenges/targets/project_id=bu4cast/drivers/pseudo")
+  s3_stage3 <- s3_read$path("challenges/project_id=bu4cast/drivers/stage3")
+  s3_pseudo  <- s3_read$path("challenges/project_id=bu4cast/drivers/pseudo")
 
   stage3_df <- arrow::open_dataset(s3_stage3) %>%
     dplyr::filter(site_id == curr_site_id) %>%

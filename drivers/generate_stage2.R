759d7ed14bf7a8d3f6e4d7b4bcae97b4a6ef6630
Cami Webb <cwebb16@bu.edu>
2026-03-04 15:21:57 -0500

    Update generate_stage2.R

--- drivers/generate_stage2.R
+++ drivers/generate_stage2.R
@@ -13,14 +13,14 @@ s3_read <- arrow::s3_bucket(
 )
 
 site_list <- arrow::read_csv_arrow(
-  s3_read$path("challenges/targets/project_id=bu4cast/field_sites.csv")
+  s3_read$path("challenges/project_id=bu4cast/metadata/field_sites.csv")
 ) %>%
   as.data.frame() %>%
   dplyr::rename(site_id = field_site_id)
 
 message("Sites loaded: ", nrow(site_list))
 
-s3_stage2 <- s3_read$path("challenges/targets/project_id=bu4cast/drivers/stage2")
+s3_stage2 <- s3_read$path("challenges/project_id=bu4cast/drivers/stage2")
 
 have_dates    <- dplyr::tibble(reference_datetime = gsub("reference_datetime=", "", s3_stage2$ls()))
 curr_date     <- Sys.Date()
@@ -34,7 +34,7 @@ if (length(missing_dates) > 0) {
 
     # Read stage1 from bu4cast bucket
     s3_stage1 <- s3_read$path(
-      paste0("challenges/targets/project_id=bu4cast/drivers/stage1/reference_datetime=", missing_dates[i])
+      paste0("challenges/project_id=bu4cast/drivers/stage1/reference_datetime=", missing_dates[i])
     )
 
     site_df <- arrow::open_dataset(s3_stage1) %>%

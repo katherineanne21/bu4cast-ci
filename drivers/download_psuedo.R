61c82efea8150f369fd9a9901148694f99e998fd
Cami Webb <cwebb16@bu.edu>
2026-03-06 10:49:44 -0500

    Update download_psuedo.R
    Update configs

--- drivers/download_psuedo.R
+++ drivers/download_psuedo.R
@@ -1,22 +1,25 @@
-## setup
+# Setup
 library(gdalcubes)
 library(gefs4cast)
 library(arrow)
 library(dplyr)
-print(sessioninfo::package_info())
-gdalcubes::gdalcubes_options(parallel = 20)
+library(yaml)
 
-# Read bucket for sites and driver storage
-s3_read <- arrow::s3_bucket(
-  "bu4cast-ci-read",
-  endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
+config <- yaml::read_yaml("challenge_configuration.yaml")
+
+gdalcubes::gdalcubes_options(parallel = 2 * parallel::detectCores())
+
+# Read bucket
+s3 <- arrow::s3_bucket(
+  config$s3_bucket_read,
+  endpoint_override = config$endpoint,
   access_key = Sys.getenv("OSN_KEY"),
   secret_key = Sys.getenv("OSN_SECRET"),
   scheme = "https"
 )
 
 sites <- arrow::read_csv_arrow(
-  s3_read$path("challenges/project_id=bu4cast/metadata/field_sites.csv")
+  s3$path(paste0(config$target_metadata_bucket, "/field_sites.csv"))
 ) %>%
   as.data.frame() %>%
   transmute(
@@ -24,15 +27,17 @@ sites <- arrow::read_csv_arrow(
     latitude  = as.numeric(latitude),
     longitude = as.numeric(longitude)
   )
-
 message("Sites loaded: ", nrow(sites))
 
 Sys.setenv("GEFS_VERSION" = "v12")
-dates        <- seq(as.Date("2020-09-24"), as.Date("2021-01-01") - 1, by = 1)
-dates_pseudo <- seq(as.Date("2020-09-24"), as.Date("2021-01-01"),     by = 1)
+
+dates_pseudo <- seq(as.Date("2020-09-24"), Sys.Date(), by = 1)
 
 message("GEFS v12 pseudo")
-s3 <- s3_read$path("challenges/project_id=bu4cast/drivers/pseudo")
-have_dates    <- gsub("reference_datetime=", "", s3$ls())
+s3_path    <- s3$path(paste0(config$drivers_bucket, "/pseudo"))
+have_dates <- tryCatch(
+  gsub("reference_datetime=", "", s3_path$ls()),
+  error = function(e) character(0)
+)
 missing_dates <- dates_pseudo[!(as.character(dates_pseudo) %in% have_dates)]
-gefs4cast:::gefs_pseudo_measures(missing_dates, path = s3, sites = sites)
+gefs4cast:::gefs_pseudo_measures(missing_dates, path = s3_path, sites = sites)

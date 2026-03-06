16f4daf0037634f5001c94212980057b05929671
Cami Webb <cwebb16@bu.edu>
2026-03-06 10:52:41 -0500

    Update generate_stage2.R
    Update configs

--- drivers/generate_stage2.R
+++ drivers/generate_stage2.R
@@ -1,27 +1,27 @@
 source("https://raw.githubusercontent.com/eco4cast/neon4cast/ci_upgrade/R/to_hourly.R")
 library(arrow)
 library(dplyr)
-print(sessioninfo::package_info())
+library(yaml)
 
-# Read bucket for sites and driver storage
-s3_read <- arrow::s3_bucket(
-  "bu4cast-ci-read",
-  endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
+config <- yaml::read_yaml("challenge_configuration.yaml")
+
+# Read bucket 
+s3 <- arrow::s3_bucket(
+  config$s3_bucket_read,
+  endpoint_override = config$endpoint,
   access_key = Sys.getenv("OSN_KEY"),
   secret_key = Sys.getenv("OSN_SECRET"),
   scheme = "https"
 )
 
 site_list <- arrow::read_csv_arrow(
-  s3_read$path("challenges/project_id=bu4cast/metadata/field_sites.csv")
+  s3$path(paste0(config$target_metadata_bucket, "/field_sites.csv"))
 ) %>%
   as.data.frame() %>%
   dplyr::rename(site_id = field_site_id)
-
 message("Sites loaded: ", nrow(site_list))
 
-s3_stage2 <- s3_read$path("challenges/project_id=bu4cast/drivers/stage2")
-
+s3_stage2 <- s3$path(paste0(config$drivers_bucket, "/stage2"))
 have_dates    <- dplyr::tibble(reference_datetime = gsub("reference_datetime=", "", s3_stage2$ls()))
 curr_date     <- Sys.Date()
 last_week     <- dplyr::tibble(reference_datetime = as.character(seq(curr_date - lubridate::days(14), curr_date - lubridate::days(1), by = "1 day")))
@@ -31,25 +31,21 @@ missing_dates <- dplyr::anti_join(last_week, have_dates, by = "reference_datetim
 if (length(missing_dates) > 0) {
   for (i in seq_along(missing_dates)) {
     print(missing_dates[i])
-
     # Read stage1 from bu4cast bucket
-    s3_stage1 <- s3_read$path(
-      paste0("challenges/project_id=bu4cast/drivers/stage1/reference_datetime=", missing_dates[i])
+    s3_stage1 <- s3$path(
+      paste0(config$drivers_bucket, "/stage1/reference_datetime=", missing_dates[i])
     )
-
     site_df <- arrow::open_dataset(s3_stage1) %>%
       dplyr::filter(variable %in% c("PRES", "TMP", "RH", "UGRD", "VGRD", "APCP", "DSWRF", "DLWRF")) %>%
       dplyr::filter(site_id %in% site_list$site_id) %>%
       dplyr::collect() %>%
       dplyr::mutate(reference_datetime = missing_dates[i])
-
     hourly_df <- to_hourly(site_df, use_solar_geom = TRUE, psuedo = FALSE) %>%
       dplyr::mutate(
         ensemble           = as.numeric(stringr::str_sub(ensemble, start = 4, end = 5)),
         reference_datetime = lubridate::as_date(reference_datetime)
       ) %>%
       dplyr::rename(parameter = ensemble)
-
     arrow::write_dataset(hourly_df, path = s3_stage2, partitioning = c("reference_datetime", "site_id"))
   }
 }

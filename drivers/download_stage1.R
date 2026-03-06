06390321ba903ed7cbb1c15692aab0e76a32908d
Cami Webb <cwebb16@bu.edu>
2026-03-06 10:47:13 -0500

    Update download_stage1.R
    Update configs

--- drivers/download_stage1.R
+++ drivers/download_stage1.R
@@ -1,22 +1,23 @@
-## setup
 library(gdalcubes)
 library(gefs4cast)
 library(arrow)
 library(dplyr)
-print(sessioninfo::package_info())
+library(yaml)
+
+config <- yaml::read_yaml("challenge_configuration.yaml")
+
 gdalcubes::gdalcubes_options(parallel = 2 * parallel::detectCores())
 
-# Read bucket for sites and driver storage
-s3_read <- arrow::s3_bucket(
-  "bu4cast-ci-read",
-  endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
+s3 <- arrow::s3_bucket(
+  config$s3_bucket_read,
+  endpoint_override = config$endpoint,
   access_key = Sys.getenv("OSN_KEY"),
   secret_key = Sys.getenv("OSN_SECRET"),
   scheme = "https"
 )
 
 sites <- arrow::read_csv_arrow(
-  s3_read$path("challenges/project_id=bu4cast/metadata/field_sites.csv")
+  s3$path(paste0(config$target_metadata_bucket, "/field_sites.csv"))
 ) %>%
   as.data.frame() %>%
   transmute(
@@ -24,37 +25,34 @@ sites <- arrow::read_csv_arrow(
     latitude  = as.numeric(latitude),
     longitude = as.numeric(longitude)
   )
-
 message("Sites loaded: ", nrow(sites))
 
 Sys.setenv("GEFS_VERSION" = "v12")
-#dates        <- seq(as.Date("2020-09-24"), Sys.Date() - 1, by = 1)
-#dates_pseudo <- seq(as.Date("2020-09-24"), Sys.Date(),     by = 1)
 
-dates        <- seq(as.Date("2020-09-24"), as.Date("2021-01-01") - 1, by = 1)
-dates_pseudo <- seq(as.Date("2020-09-24"), as.Date("2021-01-01"),     by = 1)
+dates        <- seq(as.Date("2020-09-24"), Sys.Date() - 1, by = 1)
+dates_pseudo <- seq(as.Date("2020-09-24"), Sys.Date(),     by = 1)
 
 message("GEFS v12 stage1-stats")
 bench::bench_time({
-  s3 <- s3_read$path("challenges/project_id=bu4cast/drivers/stage1-stats")
+  s3_path <- s3$path(paste0(config$drivers_bucket, "/stage1-stats"))
   have_dates <- tryCatch(
-    gsub("reference_datetime=", "", s3$ls()),
+    gsub("reference_datetime=", "", s3_path$ls()),
     error = function(e) character(0)
   )
   missing_dates <- dates[!(as.character(dates) %in% have_dates)]
   gefs_to_parquet(missing_dates,
                   ensemble = c("geavg", "gespr"),
-                  path     = s3,
+                  path     = s3_path,
                   sites    = sites)
 })
 
 message("GEFS v12 stage1")
 bench::bench_time({
-  s3 <- s3_read$path("challenges/project_id=bu4cast/drivers/stage1")
+  s3_path <- s3$path(paste0(config$drivers_bucket, "/stage1"))
   have_dates <- tryCatch(
-    gsub("reference_datetime=", "", s3$ls()),
+    gsub("reference_datetime=", "", s3_path$ls()),
     error = function(e) character(0)
   )
   missing_dates <- dates[!(as.character(dates) %in% have_dates)]
-  gefs_to_parquet(missing_dates, path = s3, sites = sites)
+  gefs_to_parquet(missing_dates, path = s3_path, sites = sites)
 })

2022fc02e3acd4ba0a7a33dcd90e8e6f742cbfd7
Cami Webb <cwebb16@bu.edu>
2026-03-13 16:19:31 -0400

    Update generate_stage3.R

--- drivers/generate_stage3.R
+++ drivers/generate_stage3.R
@@ -1,65 +1,68 @@
 library(minioclient)
-source("https://raw.githubusercontent.com/eco4cast/neon4cast/main/R/to_hourly.R")
+library(arrow)
+library(dplyr)
+library(yaml)
+source("drivers/to_hourly.R")
 
-#install_mc()
-mc_alias_set("osn", "sdsc.osn.xsede.org", "", "")
-mc_mirror("osn/bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/pseudo", "pseudo")
+config <- yaml::read_yaml("challenge_configuration.yaml")
 
-duckdbfs::duckdb_secrets(
-  endpoint = 'sdsc.osn.xsede.org',
-  key = Sys.getenv("OSN_KEY"),
-  secret = Sys.getenv("OSN_SECRET"))
+s3 <- arrow::s3_bucket(
+  config$s3_bucket_read,
+  endpoint_override = config$endpoint,
+  access_key = Sys.getenv("OSN_KEY"),
+  secret_key = Sys.getenv("OSN_SECRET"),
+  scheme = "https"
+)
 
-df <- arrow::open_dataset("pseudo") |>
-  dplyr::filter(variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF"))
+metadata_path <- gsub(paste0("^", config$s3_bucket_read, "/"), "", config$target_metadata_bucket)
+drivers_path  <- gsub(paste0("^", config$s3_bucket_read, "/"), "", config$drivers_bucket)
 
+site_coords <- arrow::read_csv_arrow(
+  s3$path(paste0(metadata_path, "/field_sites.csv"))
+) %>%
+  as.data.frame() %>%
+  dplyr::rename(site_id = field_site_id)
 
-site_list <- readr::read_csv(paste0("https://github.com/eco4cast/",
-                                    "neon4cast-noaa-download/",
-                                    "raw/master/noaa_download_site_list.csv"),
-                             show_col_types = FALSE) |> dplyr::pull(site_id)
+site_list <- site_coords$site_id
+message("Sites loaded: ", length(site_list))
 
-s3 <- arrow::s3_bucket("bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12",
-                       endpoint_override = "sdsc.osn.xsede.org",
-                       access_key= Sys.getenv("OSN_KEY"),
-                       secret_key= Sys.getenv("OSN_SECRET"))
-
-s3$CreateDir("stage3")
-
-s3 <- arrow::s3_bucket("bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/stage3",
-                       endpoint_override = "sdsc.osn.xsede.org",
-                       access_key= Sys.getenv("OSN_KEY"),
-                       secret_key= Sys.getenv("OSN_SECRET"))
-
-#site_list <- site_list[1:3]
+mc_alias_set("osn", "minio-s3.apps.shift.nerc.mghpcc.org", Sys.getenv("OSN_KEY"), Sys.getenv("OSN_SECRET"))
+mc_mirror("osn/bu4cast-ci-read/challenges/project_id=bu4cast/drivers/pseudo", "pseudo")
 
 future::plan("future::multisession", workers = 8)
+furrr::future_walk(site_list, function(curr_site_id) {
+  source("drivers/to_hourly.R")
+  library(arrow)
+  library(dplyr)
+  library(yaml)
 
-furrr::future_walk(site_list, function(curr_site_id){
+  config       <- yaml::read_yaml("challenge_configuration.yaml")
+  drivers_path <- gsub(paste0("^", config$s3_bucket_read, "/"), "", config$drivers_bucket)
 
   df <- arrow::open_dataset("pseudo") |>
-    dplyr::filter(variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF")) |>
+    dplyr::filter(variable %in% c("PRES", "TMP", "RH", "UGRD", "VGRD", "APCP", "DSWRF", "DLWRF")) |>
     dplyr::filter(site_id == curr_site_id) |>
     dplyr::collect() |>
-    dplyr::mutate(date = lubridate::as_date(reference_datetime),
+    dplyr::mutate(date         = lubridate::as_date(reference_datetime),
                   new_datetime = date + lubridate::hours(as.numeric(horizon)) + lubridate::hours(as.numeric(cycle)),
-                  datetime = ifelse(datetime != new_datetime, new_datetime, datetime),
-                  datetime = lubridate::as_datetime(datetime)) |>
+                  datetime     = ifelse(datetime != new_datetime, new_datetime, datetime),
+                  datetime     = lubridate::as_datetime(datetime)) |>
     dplyr::select(-date, -new_datetime)
 
-  s3 <- arrow::s3_bucket("bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/stage3",
-                         endpoint_override = "sdsc.osn.xsede.org",
-                         access_key= Sys.getenv("OSN_KEY"),
-                         secret_key= Sys.getenv("OSN_SECRET"))
+  s3_w <- arrow::s3_bucket(
+    config$s3_bucket_read,
+    endpoint_override = config$endpoint,
+    access_key = Sys.getenv("OSN_KEY"),
+    secret_key = Sys.getenv("OSN_SECRET"),
+    scheme = "https"
+  )
 
   print(curr_site_id)
   df |>
-    #dplyr::filter(site_id == curr_site_id) |>
-    #dplyr::collect() |>
     to_hourly(use_solar_geom = TRUE, psuedo = TRUE) |>
     dplyr::mutate(ensemble = as.numeric(stringr::str_sub(ensemble, start = 4, end = 5))) |>
     dplyr::rename(parameter = ensemble) |>
-    arrow::write_dataset(path = s3, partitioning = "site_id")
-    #duckdbfs::write_dataset(path = "s3://bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/stage3", format = 'parquet',
-    #                        partitioning = "site_id")
-})
+    arrow::write_dataset(path = s3_w$path(paste0(drivers_path, "/stage3")),
+                         partitioning = "site_id")
+},
+.options = furrr::furrr_options(globals = "site_coords"))

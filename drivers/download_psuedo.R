ad490e9f8def429daa651f690fb71017d3cbc7d1
Cami Webb <cwebb16@bu.edu>
2026-03-19 17:04:45 -0400

    Update download_psuedo.R
    Add site check for each date so adding new sites automatically updates met

--- drivers/download_psuedo.R
+++ drivers/download_psuedo.R
@@ -7,7 +7,7 @@ library(yaml)
 
 config <- yaml::read_yaml("challenge_configuration.yaml")
 
-gdalcubes::gdalcubes_options(parallel = parallel::detectCores())
+gdalcubes::gdalcubes_options(parallel = 4)
 
 # Read bucket
 s3 <- arrow::s3_bucket(
@@ -37,8 +37,15 @@ dates_pseudo <- seq(as.Date(config$gefs_start_date), Sys.Date(), by = 1)
 message("GEFS v12 pseudo")
 s3_path    <- s3$path(paste0(drivers_path, "/pseudo"))
 have_dates <- tryCatch(
-  gsub("reference_datetime=", "", s3_path$ls()),
-  error = function(e) character(0)
-)
-missing_dates <- dates_pseudo[!(as.character(dates_pseudo) %in% have_dates)]
+    gsub("reference_datetime=", "", s3_path$ls()),
+    error = function(e) character(0)
+  )
+  missing_dates <- purrr::keep(as.character(dates), function(d) {
+    if (!(d %in% have_dates)) return(TRUE)
+    have_sites <- tryCatch(
+      gsub("site_id=", "", s3_path$path(paste0("reference_datetime=", d))$ls()),
+      error = function(e) character(0)
+    )
+    !all(as.character(sites$site_id) %in% have_sites)
+  })
 gefs4cast:::gefs_pseudo_measures(missing_dates, path = s3_path, sites = sites)

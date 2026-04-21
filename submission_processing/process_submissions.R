6b1f9634bc3e4026eca9bfcb9634e4c14664053b
Katherine Anne <157232885+katherineanne21@users.noreply.github.com>
2026-04-21 12:15:47 -0400

    Update process_submissions.R

--- submission_processing/process_submissions.R
+++ submission_processing/process_submissions.R
@@ -50,7 +50,7 @@ print("Submissions Bucket")
 print(config$submissions_bucket)
 
 minioclient::mc_cp(
-  from = paste0(config$s3_bucket_write, config$submissions_bucket),
+  from = paste0(config$s3_bucket_write, config$submissions_bucket, "/"),
   to   = local_dir,
   recursive = TRUE
 )
